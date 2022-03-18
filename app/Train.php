d5370b249135252c8f93dbd7012a298eaa1d7c91
Sasha Mainardi <sashamainardi4@gmail.com>
2022-03-18 17:25:07 +0100

    seeder completed

--- app/Train.php
+++ app/Train.php
@@ -7,4 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Train extends Model
 {
+    protected $fillable = [
+        [
+            'Azienda',
+            'Stazione_di_partenza',
+            'Stazione_di_arrivo',
+            'Orario_di_partenza',
+            'Orario_di_arrivo',
+            'Codice_treno',
+            'Carrozze',
+            'In_orario',
+            'Cancellato'
+        ]
+    ];
 }
--- database/seeds/TrainsTableSeeder.php
+++ database/seeds/TrainsTableSeeder.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Seeder;
 use App\Train;
 
 
+
 class TrainsTableSeeder extends Seeder
 {
     /**
@@ -13,18 +14,40 @@ class TrainsTableSeeder extends Seeder
      */
     public function run()
     {
-        $new_train = new Train();
+        $trains = [
+            [
+                'Azienda' => 'Vd Trains',
+                'Stazione_di_partenza' => 'Milano Centrale',
+                'Stazione_di_arrivo' => 'Brescia',
+                'Orario_di_partenza' => '2022-02-10 10:35:21',
+                'Orario_di_arrivo' => '2022-02-10 10:41:25',
+                'Codice_treno' => 452,
+                'Carrozze' => 32,
+                'In_orario' => true,
+                'Cancellato' => false,
+            ],
 
-        $new_train->Azienda = 'ciao';
-        $new_train->Stazione_di_partenza = 'Milano';
-        $new_train->Stazione_di_arrivo = 'Brescia';
-        $new_train->Orario_di_partenza = '2022-02-10 10:35:21';
-        $new_train->Orario_di_arrivo = '2022-02-10 10:41:25';
-        $new_train->Codice_treno = 445;
-        $new_train->Carrozze = 20;
-        $new_train->In_orario = true;
-        $new_train->Cancellato = false;
+            [
+                'Azienda' => 'aks transport',
+                'Stazione_di_partenza' => 'Milano rogorego',
+                'Stazione_di_arrivo' => 'Torino',
+                'Orario_di_partenza' => '2022-02-12 14:35:21',
+                'Orario_di_arrivo' => '2022-02-10 14:41:25',
+                'Codice_treno' => 556,
+                'Carrozze' => 21,
+                'In_orario' => false,
+                'Cancellato' => false,
+            ]
 
-        $new_train->save();
+        ];
+
+
+        foreach ($trains as $train) {
+            $new_train = new Train();
+
+            $new_train->fill($train);
+
+            $new_train->save();
+        }
     }
 }
