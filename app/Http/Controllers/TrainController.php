fef1a1931645bb3e8d9b492db8ce1b0a3c735a10
Sasha Mainardi <sashamainardi4@gmail.com>
2022-03-18 17:37:49 +0100

    added page and route

---
+++ app/Http/Controllers/TrainController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Train;
+use Illuminate\Http\Request;
+
+class TrainController extends Controller
+{
+    public function index()
+    {
+        $trains = Train::all();
+        dd($trains);
+        return view('home');
+    }
+}
