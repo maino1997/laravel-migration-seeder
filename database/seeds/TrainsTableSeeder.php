9ecb96c0f92dab7a8d452f349be02f0f4441e754
Sasha Mainardi <sashamainardi4@gmail.com>
2022-03-18 17:53:38 +0100

    trains array in config

--- database/seeds/TrainsTableSeeder.php
+++ database/seeds/TrainsTableSeeder.php
@@ -4,7 +4,6 @@ use Illuminate\Database\Seeder;
 use App\Train;
 
 
-
 class TrainsTableSeeder extends Seeder
 {
     /**
@@ -14,32 +13,8 @@ class TrainsTableSeeder extends Seeder
      */
     public function run()
     {
-        $trains = [
-            [
-                'Azienda' => 'Vd Trains',
-                'Stazione_di_partenza' => 'Milano Centrale',
-                'Stazione_di_arrivo' => 'Brescia',
-                'Orario_di_partenza' => '2022-02-10 10:35:21',
-                'Orario_di_arrivo' => '2022-02-10 10:41:25',
-                'Codice_treno' => 452,
-                'Carrozze' => 32,
-                'In_orario' => true,
-                'Cancellato' => false,
-            ],
 
-            [
-                'Azienda' => 'aks transport',
-                'Stazione_di_partenza' => 'Milano rogorego',
-                'Stazione_di_arrivo' => 'Torino',
-                'Orario_di_partenza' => '2022-02-12 14:35:21',
-                'Orario_di_arrivo' => '2022-02-10 14:41:25',
-                'Codice_treno' => 556,
-                'Carrozze' => 21,
-                'In_orario' => false,
-                'Cancellato' => false,
-            ]
-
-        ];
+        $trains = config('trains');
 
 
         foreach ($trains as $train) {
